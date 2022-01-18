6fbc88f969302d335e7194e41b8e6f267bd5a6a7
jzl011 <64996628+jzl011@users.noreply.github.com>
2022-01-17 20:42:39 -0800

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shopify_backend.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Summary { get; set; }
+
+        //Relations
+        //Relations
+        //public ICollection<Purchase> Purchases { get; set; }
+    }
+}
